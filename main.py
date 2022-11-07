b43b744495bf5b7e587f683a3b41316d7fb310aa
jmuriki <106560016+jmuriki@users.noreply.github.com>
2022-11-07 19:59:49 +0300

    Post comix to the wall
    - Add post_to_the_wall
    - Split fetch_comix
    - Add compile_comix_url
    - Refine compose_filepath
    - Dry save_content
    - Dry upload_to_server
    - Dry variables names

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ from urllib.parse import urlparse
 from urllib.parse import unquote
 
 
+def compile_comix_url(comix_id):
+    xkcd_url = "https://xkcd.com/"
+    comix_url = f"{xkcd_url}{comix_id}/info.0.json"
+    return comix_url
+
+
 def get_comix_meta(url):
     repsonse = requests.get(url)
     repsonse.raise_for_status()
@@ -17,26 +23,26 @@ def compose_filepath(url):
     url_part = urlparse(url).path
     unquoted_url_part = unquote(url_part)
     name = os.path.split(unquoted_url_part)[-1]
-    return f"./{name}"
+    return Path(f"./{name}")
 
 
 def save_content(url, path):
     response = requests.get(url)
     response.raise_for_status()
-    with open(Path(path), "wb") as file:
+    with open(path, "wb") as file:
         file.write(response.content)
+        return path
 
 
 def fetch_comix():
-    xkcd_url = "https://xkcd.com/"
     comix_id = 353
-    comix_url = f"{xkcd_url}{comix_id}/info.0.json"
+    comix_url = compile_comix_url(comix_id)
     comix_meta = get_comix_meta(comix_url)
-    img_url = comix_meta["img"]
-    img_path = compose_filepath(img_url)
-    save_content(img_url, img_path)
-    print(comix_meta["alt"])
-    return img_path
+    image_url = comix_meta["img"]
+    image_path = compose_filepath(image_url)
+    image = save_content(image_url, image_path)
+    text = comix_meta["alt"]
+    return image, text
 
 
 def get_upload_url(api_url, params):
@@ -47,19 +53,16 @@ def get_upload_url(api_url, params):
     return response.json()["response"]["upload_url"]
 
 
-def upload_to_server(upload_url, group_id, pic_path):
-    with open(Path(pic_path), "rb") as file:
+def upload_to_server(upload_url, image):
+    with open(image, "rb") as file:
         method = "photos.saveWallPhoto"
         files = {
             "photo": file,
         }
-        params = {
-            "group_id": group_id,
-        }
         method_url = f"{upload_url}{method}"
-        response = requests.post(method_url, files=files, params=params)
+        response = requests.post(method_url, files=files)
         response.raise_for_status()
-    return response.json()
+        return response.json()
 
 
 def save_to_album(api_url, params, upload_response):
@@ -68,23 +71,41 @@ def save_to_album(api_url, params, upload_response):
     method_url = f"{api_url}method/{method}"
     response = requests.get(method_url, params=params)
     response.raise_for_status()
-    print(response.json())
+    return response.json()
+
+
+def post_to_the_wall(api_url, params, save_response, group_id, text):
+    method = "wall.post"
+    method_url = f"{api_url}method/{method}"
+    owner_id = save_response["response"][0]["owner_id"]
+    media_id = save_response["response"][0]["id"]
+    params["owner_id"] = f"-{group_id}"
+    params["message"] = text
+    params["from_group"] = 1
+    params["attachments"] = f"photo{owner_id}_{media_id}"
+    response = requests.get(method_url, params=params)
+    response.raise_for_status()
+
+
+def publish_to_vk(group_id, token, image, text):
+    api_url = "https://api.vk.com/"
+    api_version = 5.131
+    params = {
+        "access_token": token,
+        "v": api_version,
+    }
+    upload_url = get_upload_url(api_url, params)
+    upload_response = upload_to_server(upload_url, image)
+    save_response = save_to_album(api_url, params, upload_response)
+    post_to_the_wall(api_url, params, save_response, group_id, text)
 
 
 def main():
     load_dotenv()
-    access_token = os.getenv("VK_ACCESS_TOKEN")
     group_id = os.getenv("VK_GROUP_ID")
-    api_url = "https://api.vk.com/"
-    api_version = 5.131
-    params = {
-        "access_token": access_token,
-        "v": api_version,
-    }
-    pic_path = fetch_comix()
-    upload_url = get_upload_url(api_url, params)
-    upload_response = upload_to_server(upload_url, group_id, pic_path)
-    save_to_album(api_url, params, upload_response)
+    token = os.getenv("VK_ACCESS_TOKEN")
+    image, text = fetch_comix()
+    publish_to_vk(group_id, token, image, text)
 
 
 if __name__ == "__main__":
